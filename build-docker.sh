36fdd7ed5222e2f2b8acaa7882140d9c9297ae26
Fahad Yousuf <fyousuf@centos-7-host.gplab.fy.loc>
2023-01-30 22:04:45 +0300

    Add podman detectoin to the container build script.

--- build-docker.sh
+++ build-docker.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 TS=$(date +%Y%m%d%H%M%S) 
+
+if command -v podman 
+then
+  DOCKER_CMD="podman"
+elif command -v docker
+then
+  DOCKER_CMD="docker"
+else
+  echo "Docker or Podman not found. Exiting"
+  exit 1
+fi
+
 # Build
-docker build . -t fahadysf/gptool:2.7-dev -t fahadysf/gptool:2.7-$TS -t fahadysf/gptool:2.7-latest
+$DOCKER_CMD build . -t fahadysf/gptool:2.7-dev -t fahadysf/gptool:2.7-$TS -t fahadysf/gptool:2.7-latest
 # Push
-docker push fahdaysf/gptool:2.7-dev
-docker push fahadysf/gptool:2.7-$TS
-docker push fahadysf/gptool:2.7-latest
+$DOCKER_CMD push fahdaysf/gptool:2.7-dev
+$DOCKER_CMD push fahadysf/gptool:2.7-$TS
+$DOCKER_CMD push fahadysf/gptool:2.7-latest
