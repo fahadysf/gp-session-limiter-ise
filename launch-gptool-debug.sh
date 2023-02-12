acf230a35ca31eefab302d31e7f3f3ca684a4f7a
Fahad Yousuf <fahadysf@gmail.com>
2023-02-12 12:16:07 +0000

    Duplicate Criteria update.

---
+++ launch-gptool-debug.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEBUG=1
+CONFIG_FILE="/opt/gptool/config.yaml"
+DATA_FOLDER="/opt/gptool/data"
+LOG_FOLDER="/opt/gptool/logs"
+
+docker run -itd --rm --name gptool \
+	-e "DEBUG=$DEBUG" \
+	-v $CONFIG_FILE:/app/config.yaml \
+	-v $DATA_FOLDER:/app/data \
+	-v $LOG_FOLDER:/app/logs \
+	-p 8000:8000 \
+	fahadysf/gptool:2.7-latest
